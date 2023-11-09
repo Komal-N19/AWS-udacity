eeff33f48c30943a5fdc718dd0bf86cae1063bb8
Komal Nangia <133215753+Komal-N19@users.noreply.github.com>
2023-11-09 23:23:03 +0530

    int key, fixed error

--- classify_images.py
+++ classify_images.py
@@ -75,8 +75,8 @@ def classify_images(images_dir, results_dic, model):
     #defining empty dictionary
     
     for key in results_dic:
-        path_filename = key['images_dir']
-        model = key['model']
+        path_filename = results_dic[int(key)]['images_dir']
+        model = results_dic[int(key)]['model']
         model_label = classifier(path_filename,model)
         model_label = model_label.lower().strip()
         
